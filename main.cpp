1b0dc8a6e163eb6b8864628685dff94a661ad2a6
dax <dax26d@gmail.com>
2024-08-06 13:44:09 +0800

    重新调整项目的结构

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "headers.h"
-
-int main(int argc, char *argv[])
-{
-	dax::test_foldexpr();
-	return 0;
-}
--- src/foldexpression/foldexpression.cpp
+++ src/foldexpression/foldexpression.cpp
@@ -4,9 +4,10 @@ void dax::test_foldexpr()
 {
 	dax::print("hello", "world", 22);
 	dax::print_limits<unsigned long, unsigned long long, unsigned, signed, int, float, double, long double, long, long long, char, short>();
-	dax::print("hello", "world", 23);
+}
 
-	std::vector<int> ivec{};
-	// push_back(ivec, 1, 2, 3);
-	// print(ivec[2]);
+int main(int argc, char *argv[])
+{
+	dax::test_foldexpr();
+	return 0;
 }
